f7eba1b520353d70e96ae56dfee92b09b46a0681
Kofirs2634 <artemiy27072004@gmail.com>
2021-12-27 17:08:19 +0300

    many things; preparing to make relations

--- gui/script.js
+++ gui/script.js
@@ -1,15 +1,46 @@
 /**
+ * Описывает один компонент
  * @typedef Component
- * @property {string} id
- * @property {string} name
+ * @property {string} id Идентификатор компонента
+ * @property {string} name Название компонента
+ */
+
+/**
+ * Описывает отношения компонентов
+ * @typedef Relation
+ * @property {string} parent Идентификатор родителя
+ * @property {string} child Идентификатор потомка
+ * @property {number} amount Количество потомков для составления родителя
  */
 
  /**
+  * Описывает компонент, добавленный в заказ
   * @typedef OrderItem
-  * @property {string} id
-  * @property {number} amount
+  * @property {string} id Идентификатор компонента
+  * @property {number} amount Количество единиц в заказе
   */
 
+/**
+ * Создает случайный идентификатор компонента
+ * @returns Строка-идентификатор
+ */
+function generateIdentifier() {
+    return 'xxxx-xxxx'.replace(/x/g, () => Math.floor(Math.random() * 0x10).toString(0x10))
+}
+
+/**
+ * Экранирует спецсимволы регулярного отношения
+ * @param {string} string Обрабатываемая строка
+ * @returns Экранированная строка
+ */
+function escapeRegexp(string) {
+    return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&')
+}
+
+/**
+ * Точка входа в интерфейс приложения. Используется самовызывающаяся
+ * функция для защиты данных от любопытных юзеров
+ */
 ;(function() {
     /**
      * Обновляет список компонентов
@@ -28,6 +59,7 @@
                 if (!event.shiftKey) {
                     const modal = openModal('#listing-modal')
                     modal.querySelector('#listing-modal-id').value = e.id
+                    modal.querySelector('#listing-modal-id').setAttribute('disabled', '')
                     modal.querySelector('#listing-modal-name').value = e.name
                     modal.setAttribute('action', 'edit')
                     modal.setAttribute('oid', e.id)
@@ -53,7 +85,14 @@
             _t.querySelector('.order-name').innerText = listing.find(f => f.id == e.id).name
             _t.querySelector('.order-amount').innerText = `(${e.amount} шт.)`
             _t.querySelector('.item').addEventListener('click', event => {
-                if (event.shiftKey) {
+                if (!event.shiftKey) {
+                    const modal = openModal('#order-modal')
+                    fillComponentSelection(listing, '#order-item-name')
+                    modal.querySelector('#order-item-name').value = e.id
+                    modal.querySelector('#order-item-amount').value = e.amount
+                    modal.setAttribute('action', 'edit')
+                    modal.setAttribute('oid', e.id)
+                } else {
                     order = removeListEntry(event.target, order, e.id)
                     updateOrder()
                 }
@@ -83,6 +122,20 @@
     }
 
     /**
+     * Заполняет выпадающее меню компонентами
+     * @param {Component[]} list Список компонентов
+     * @param {string} selector Селектор выпадающего меню
+     */
+    function fillComponentSelection(list, selector) {
+        list.forEach(e => {
+            const _t = document.createElement('option')
+            _t.setAttribute('value', e.id)
+            _t.innerText = e.name
+            document.querySelector(selector).append(_t)
+        })
+    }
+
+    /**
      * Открывает указанную модаль
      * @param {string} selector Селектор элемента модали
      * @returns Указанная модаль для дальнейшего взаимодейтвия с ней
@@ -91,6 +144,7 @@
         const modal = document.querySelector(selector)
         modal.classList.add('active')
         modal.setAttribute('action', 'new')
+        modal.querySelectorAll('input').forEach(e => e.removeAttribute('disabled'))
         document.body.classList.add('shadowed')
         return modal
     }
@@ -101,6 +155,7 @@
      */
     function closeModal(modal) {
         modal.removeAttribute('action')
+        modal.removeAttribute('oid')
         modal.querySelectorAll('input').forEach(e => e.value = e.getAttribute('value') ?? '')
         modal.querySelectorAll('option').forEach(e => e.remove())
         modal.classList.remove('active')
@@ -162,13 +217,15 @@
                 switch (modalId) {
                     case 'listing': {  // запись в листинг
                         const obj = {
-                            id: modal.querySelector('[id*="id"]').value,
+                            id: modal.querySelector('[id*="id"]').value || generateIdentifier(),
                             name: modal.querySelector('[id*="name"]').value
                         }
+
                         if (action == 'new') listing.push(obj)
                         else if (action == 'edit') {
-                            listing = listing.filter(e => e.id != modal.getAttribute('oid'))
-                            listing.push(obj)
+                            const index = listing.findIndex(e => e.id == modal.getAttribute('oid'))
+                            listing[index] = obj
+                            modal.removeAttribute('oid')
                         }
                         break
                     }
@@ -177,9 +234,19 @@
                             id: modal.querySelector('[id*="name"]').value,
                             amount: parseInt(modal.querySelector('[id*="amount"]').value)
                         }
-                        const exist = order.find(e => e.id == obj.id)
-                        if (exist) exist.amount += obj.amount
-                        else order.push(obj)
+                        if (action == 'new') {
+                            const exist = order.find(e => e.id == obj.id)
+                            if (exist) exist.amount += obj.amount
+                            else order.push(obj)
+                        } else if (action == 'edit') {
+                            const index = order.findIndex(e => e.id == modal.getAttribute('oid'))
+                            const exist = order.find((e, n) => e.id == obj.id && n != index)
+                            if (exist) {
+                                exist.amount += obj.amount
+                                order = order.filter((_, n) => n != index)
+                            } else order[index] = obj
+                            modal.removeAttribute('oid')
+                        }
                         break
                     }
                 }
@@ -191,10 +258,34 @@
         }
     }
 
+    /**
+     * Фильтрует компоненты по названию
+     * @param {string} query Поисковый запрос 
+     */
+    function filterNames(query) {
+        const regexp = query ? new RegExp(escapeRegexp(query), 'i') : /.+/
+        const filtrate = listing.filter(e => e.name.match(regexp) || e.id.match(regexp))
+        document.querySelectorAll('#order-item-name option').forEach(el => el.remove())
+        if (!filtrate.length) {
+            document.querySelector('#order-modal #modal-confirm').setAttribute('disabled', '')
+            return
+        } else {
+            fillComponentSelection(filtrate, '#order-item-name')
+            document.querySelector('#order-modal #modal-confirm').removeAttribute('disabled')
+        }
+    }
+
+
+
+
 
     // создаем хранилища списков
     /** @type {Component[]} */
     var listing = interactLC('read', 'listing') ?? []
+
+    /** @type {Relation[]} */
+    var relations = interactLC('read', 'relations') ?? []
+
     /** @type {OrderItem[]} */
     var order = interactLC('read', 'order') ?? []
 
@@ -215,21 +306,17 @@
 
         // вот это вот у нас заполнение дропдауна во избежание
         // цирка с конями со стороны юзера
-        listing.forEach(e => {
-            const _t = document.createElement('option')
-            _t.setAttribute('value', e.id)
-            _t.innerText = e.name
-            modal.querySelector('#order-item-name').append(_t)
-        })
+        fillComponentSelection(listing, '#order-item-name')
     })
 
-    // навешиваем обработку кликов на управляющие кнопки модалей
+    // навешиваем обработку кликов на некоторые элементы модалей
     document.querySelectorAll('.modal-controls button').forEach(el => el.addEventListener('click', handleModalControls))
+    document.querySelector('#order-item-filter').addEventListener('input', e => filterNames(e.target.value))
 
     document.body.addEventListener('keydown', e => {
         if (e.code == 'KeyD') {
             console.log(listing, order)
-        } else if (e.code == 'KeyS') {
+        } else if (e.code == 'Period') {
             interactLC('write')
         }
     })
